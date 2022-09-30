0ffe86ccb49826cc1e0f186d16f31d1f1a53c3fa
Stefanus Likardi <stefanus.likardi@nutrifood.co.id>
2022-09-30 11:24:32 +0700

    [add] adding boostrap 5.2

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -2,18 +2,27 @@
 <html lang="en">
 
 <head>
+    <!-- Required meta tags -->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <title>@yield('page-title')</title>
 </head>
 
-<body style="display: flex;">
-  <div>
-    @include('sidebar')
-  </div>
-  <div>     
-    @yield('content')
+<body>
+  <div style="display: flex;">
+    <div>
+      @include('sidebar')
+    </div>
+    <div>     
+      @yield('content')
+    </div>
   </div>
 </body>
 
