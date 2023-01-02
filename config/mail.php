23c9de89cdd7f1727565b369580140db466e04d3
Zinct2 <christosalim0@gmail.com>
2023-01-02 14:28:21 +0700

    change mail from name

--- config/mail.php
+++ config/mail.php
@@ -93,7 +93,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'EasyBengkel@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'EasyBengkel'),
+        'name' => env('MAIL_FROM_NAME', 'System'),
     ],
 
     /*
